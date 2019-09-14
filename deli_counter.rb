f65ee92b4ee63bd0e4a7f87423ac4d1ccc831376
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-14 20:22:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    puts "The line is currently:"
     katz_deli.each_with_index do |ele, index|
-      puts "The line is currently: #{index + 1}. #{ele}"
+      puts "#{index + 1}. #{ele}"
     end
   end
   
